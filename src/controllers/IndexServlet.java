27a0e40e19c03fc31fc860862e0586b7ae2369a4
tsunodataisuke <tsunodataisuke@192.168.11.5>
2021-10-24 21:57:00 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -41,7 +41,16 @@ public class IndexServlet extends HttpServlet {
 
         request.setAttribute("messages", messages);
 
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+
+
+        }
+        // ビューとなるJSPを指定して表示する
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
+        }
     }
-}
